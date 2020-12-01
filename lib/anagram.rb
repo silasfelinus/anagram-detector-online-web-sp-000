e6445b29d9e8b496ae2a1d8ccdde2ec9baa18afe
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-01 00:04:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,10 @@ class Anagram
 
   def initialize(word)
     @word = word
-
   end
 
   def match(word_array)
-
+    word_array.detect {|test_word| test_word.split("").sort == @word.split("").sort}
   end
 
 
